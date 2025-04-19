728aa234a4bf965c1bfe0e8a1159c102a127bcc6
zengxx <2848266713@qq.com>
2025-04-19 21:51:37 +0800

    提交测试

---
+++ eslint.config.js
@@ -0,0 +1,40 @@
+import { defineConfig, globalIgnores } from 'eslint/config'
+import globals from 'globals'
+import js from '@eslint/js'
+import pluginVue from 'eslint-plugin-vue'
+import skipFormatting from '@vue/eslint-config-prettier/skip-formatting'
+
+export default defineConfig([
+  {
+    name: 'app/files-to-lint',
+    files: ['**/*.{js,mjs,jsx,vue}']
+  },
+
+  globalIgnores(['**/dist/**', '**/dist-ssr/**', '**/coverage/**']),
+
+  {
+    languageOptions: {
+      globals: {
+        ...globals.browser
+      }
+    }
+  },
+
+  js.configs.recommended,
+  ...pluginVue.configs['flat/essential'],
+  skipFormatting,
+  {
+    rules: {
+      // 使用插件提供的规则（格式：`插件名/规则名`）
+      'vue/multi-word-component-names': [
+        'warn',
+        {
+          ignores: ['index'] // vue组件名称多单词组成（忽略index.vue）
+        }
+      ],
+      'vue/no-setup-props-destructure': ['off'], // 关闭 props 解构的校验
+      // 💡 添加未定义变量错误提示，create-vue@3.6.3 关闭，这里加上是为了支持下一个章节演示。
+      'no-undef': 'error'
+    }
+  }
+])
